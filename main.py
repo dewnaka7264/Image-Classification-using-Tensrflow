717a41583c9ffe615ba151cca568db345934e540
Dewnaka <lithilakavi@gmail.com>
2024-08-18 13:43:50 +0530

    Normalized the data

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ import tensorflow as tf;
 from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
 
+# Loading and Preprocessing the Data
 (train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
+
+#Normalizing the Data
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+
