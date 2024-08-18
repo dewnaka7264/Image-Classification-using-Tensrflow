2a3af41e8d68c962fe4206c5d2c8efb5deb9504c
Dewnaka <lithilakavi@gmail.com>
2024-08-18 13:47:32 +0530

    Convolutional neural network created

--- main.py
+++ main.py
@@ -10,4 +10,19 @@ import matplotlib.pyplot as plt
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+
+
+
+
 
