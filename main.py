c67942d6a7971557892f7efd080f3dad52696d68
Dewnaka <lithilakavi@gmail.com>
2024-08-18 13:42:06 +0530

    Added the dataset and divided into training and testing sets

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 import tensorflow as tf;
+
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+
+(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
